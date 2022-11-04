57a4d93d4cb83929cece8b838251b70a165266b8
shohanurshihab <skshihab531@gmail.com>
2022-11-05 05:59:56 +0600

    dash&sidebar

--- Teacher_Mid/resources/views/inc/side.blade.php
+++ Teacher_Mid/resources/views/inc/side.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Somadhan</title>
 
+
     
   </head>
   <body>
@@ -17,12 +18,10 @@
       </label>
       <div class="left_area">
         <h3>Soma<span>dhan</span></h3>
-       
       </div>
      
-    
     </header>
- 
+
     <!--header area end-->
     <!--sidebar start-->
     <div class="sidebar">
@@ -30,43 +29,9 @@
         <img src="" class="profile_image" alt="">
         <h4>Teacher</h4>
       </center>
-      
       <a href="#"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
       <a href="#"><i class="fas fa-cogs"></i><span>Profile</span></a>
       <a href="#"><i class="fas fa-bell"></i><span>Notification</span></a>
       <a href="#"><i class="fas fa-info-circle"></i><span>About</span></a>
       <a href="#"><i class="fas fa-sign-out-alt"></i><span>Logout</span></a>
     </div>
-
-    <!--sidebar end-->
-
- 
-  
-    <div class="content"> 
-     <h2>Dashboard</h2> 
-    <div class="container">   
-  
-    
-    <br>
-     <a href="#" class="box">
-     20 <br><br>
-     Question Pending</a> 
-     
-      <a href="#" class="box">
-      6 <br><br>
-      Appointments</a> 
-      
-      <a href="#" class="box">
-      18 <br><br> 
-      Question Answered</a> 
-      
-    
-    </div>
-
-    </div>
-
-  </body>
-  <link rel="stylesheet" href="/css/side.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-    <link rel="stylesheet" href="/css/flex.css">
-</html>
--- Teacher_Mid/resources/views/layouts/app.blade.php
+++ Teacher_Mid/resources/views/layouts/app.blade.php
@@ -1,13 +1,14 @@
 <html>
     <head>
-        <title>Laravel Development Limited</title>
+        <title>Somadhan</title>
     </head>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="/css/side.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
     <body>
-        <div class=" ">
-            <div>
+    
+        @include('inc.side')
+         
                 @yield('content')
-            </div>
-        </div>
+
     </body>
     </html>
